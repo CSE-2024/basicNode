9bf35f85dfcc033f118d683a61f07316fc8f9276
CSE-2024 <csoftware2024@gmail.com>
2024-09-21 16:52:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello from Node.js backend!');
 });
 
-// Start the server only if this file is run directly
 if (require.main === module) {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
 
-module.exports = app; // Export the app for testing
+module.exports = app;
