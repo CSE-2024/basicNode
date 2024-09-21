8a1255f019bfbbf0588c54e1fbbeb01a310cdfd9
CSE-2024 <csoftware2024@gmail.com>
2024-09-21 16:52:42 -0400

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,10 @@
-const request = require('supertest'); // Import Supertest
-const app = require('../server');       // Import the Express app
+const request = require('supertest');
+const app = require('../server');
 
-// Test suite
 describe('GET /', () => {
   it('should respond with Hello from Node.js backend!', async () => {
-    const response = await request(app).get('/'); // Make a GET request
-    expect(response.status).toBe(200);           // Check status code
-    expect(response.text).toBe('Hello from Node.js backend!'); // Check response text
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello from Node.js backend!');
   });
 });
